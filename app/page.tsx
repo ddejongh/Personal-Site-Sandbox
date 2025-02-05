40bba37ef7a6041189671f1b44627ca0b930ba97
Devin DeJongh <devindejongh@gmail.com>
2025-02-04 22:02:55 -0600

    setting up primary components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import About from "@/components/About";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="bg-[#2C514C] h-screen">
-      
+      <About />
     </div>
   );
 }
