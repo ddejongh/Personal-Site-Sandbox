bd931eea9adb009a78627824b71d503b65a31d42
Devin DeJongh <devindejongh@gmail.com>
2025-02-08 15:00:27 -0600

    fixed spacing

--- components/ResumeBullet.tsx
+++ components/ResumeBullet.tsx
@@ -1,12 +1,12 @@
 export default function ResumeBullet() {
     return (
         <div>
-            <div className="grid grid-cols-5">
-                <div className="cols-span-1">
-                    2018-2019
+            <div className="grid grid-cols-5 gap-4 my-5">
+                <div className="col-span-1">
+                    <p>2018-2019</p>
                 </div>
-                <div className="cols-span-4">
-                    Here is some shit I did
+                <div className="col-span-4">
+                    <p>Here is some shit I did</p>
                 </div>
             </div>
         </div>
--- components/ResumeCard.tsx
+++ components/ResumeCard.tsx
@@ -3,11 +3,13 @@ import ResumeBullet from "./ResumeBullet";
 export default function ResumeCard() {
     return (
         <div>
-            <p>This is a summary of my experience</p>
-            <li><ResumeBullet /></li>
-            <li><ResumeBullet /></li>
-            <li><ResumeBullet /></li>
-            <li><ResumeBullet /></li>
+            <p className='mr-4 mb-4'>This is a summary of my experience</p>
+            <ul>
+                <li><ResumeBullet /></li>
+                <li><ResumeBullet /></li>
+                <li><ResumeBullet /></li>
+                <li><ResumeBullet /></li>
+            </ul>
         </div>
     )
 }
